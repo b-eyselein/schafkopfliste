0808749d35608854ed5520d9794bc3aaf43fa05e
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2020-03-03 09:30:10 +0100

    update down for migration three

--- migrations/2020-03-02-180102_session_dates_update/down.sql
+++ migrations/2020-03-02-180102_session_dates_update/down.sql
@@ -4,7 +4,7 @@ alter table sessions
     drop column date_year,
     drop column date_month,
     drop column date_day_of_month,
-    add column date date not null,
+    add column date date not null default '2020-02-29',
     drop column time_hours,
     drop column time_minutes,
-    add column time time not null;
+    add column time time not null default '12:00';
