486d15ee2b7aafdcb6ca65b50f3a352f75f53992
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2021-02-25 11:03:10 +0100

    deleted old ts interface

--- ui/src/app/_interfaces/interfaces.ts
+++ ui/src/app/_interfaces/interfaces.ts
@@ -3,46 +3,11 @@
  */
 export type BavarianSuit = 'Acorns' | 'Leaves' | 'Hearts' | 'Bells';
 
-/**
- * @deprecated
- */
-export interface CompleteSession {
-  dateDayOfMonth: number;
-  dateMonth: number;
-  dateYear: number;
-  firstPlayer: Player;
-  fourthPlayer: Player;
-  group: Group;
-  hasEnded: boolean;
-  id: number;
-  playedGames: PricedGame[];
-  ruleSet: RuleSet;
-  secondPlayer: Player;
-  sessionResults: SessionResult[] | undefined;
-  thirdPlayer: Player;
-  timeHours: number;
-  timeMinutes: number;
-}
 
 /**
  * @deprecated
  */
 export type CountLaufende = 'Always' | 'OnlyLosers' | 'Never';
-
-/**
- * @deprecated
- */
-export interface CreatableSession {
-  dateDayOfMonth: number;
-  dateMonth: number;
-  dateYear: number;
-  firstPlayerId: number;
-  fourthPlayerId: number;
-  secondPlayerId: number;
-  thirdPlayerId: number;
-  timeHours: number;
-  timeMinutes: number;
-}
 
 /**
  * @deprecated
@@ -77,29 +42,11 @@ export type GameType =
   | 'Farbwenz'
   | 'Farbgeier';
 
-/**
- * @deprecated
- */
-export interface Group {
-  id: number;
-  name: string;
-  ruleSetId: number;
-}
 
 /**
  * @deprecated
  */
 export type KontraType = 'Kontra' | 'Re' | 'Supra' | 'Resupra';
-
-/**
- * @deprecated
- */
-export interface Player {
-  abbreviation: string;
-  id: number;
-  name: string;
-  pictureName: string | undefined;
-}
 
 /**
  * @deprecated
@@ -112,54 +59,4 @@ export interface PricedGame {
 /**
  * @deprecated
  */
-export interface RuleSet {
-  basePrice: number;
-  bettelAllowed: boolean;
-  countLaufende: CountLaufende;
-  farbGeierAllowed: boolean;
-  farbWenzAllowed: boolean;
-  geierAllowed: boolean;
-  hochzeitAllowed: boolean;
-  id: number;
-  laufendePrice: number;
-  maxLaufendeIncl: number;
-  minLaufendeIncl: number;
-  name: string;
-  ramschAllowed: boolean;
-  soloPrice: number;
-}
-
-/**
- * @deprecated
- */
 export type SchneiderSchwarz = 'Schneider' | 'Schwarz';
-
-/**
- * @deprecated
- */
-export interface Session {
-  creatorUsername: string;
-  dateDayOfMonth: number;
-  dateMonth: number;
-  dateYear: number;
-  firstPlayerId: number;
-  fourthPlayerId: number;
-  groupId: number;
-  hasEnded: boolean;
-  id: number;
-  secondPlayerId: number;
-  thirdPlayerId: number;
-  timeHours: number;
-  timeMinutes: number;
-}
-
-/**
- * @deprecated
- */
-export interface SessionResult {
-  groupId: number;
-  playerId: number;
-  result: number;
-  sessionId: number;
-}
-
